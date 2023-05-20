e3f3d2f196c331404c961293c85b7784aec123af
KeltonSterett <keltonsterett24@gmail.com>
2023-05-19 18:01:16 -0600

    added functions for view all departments and roles

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const connection = require('connection');
+const connection = require('./connection.js');
 const { prompt } = require('inquirer');
 const db = require('./db/index.js');
 
@@ -49,41 +49,43 @@ function questions() {
                 case 'viewAllDepartments':
                     viewAllDepartments();
                     break;
-             // if the choice is view all roles, call the view all roles method
+                // if the choice is view all roles, call the view all roles method
                 case 'viewAllRoles':
                     viewAllRoles();
                     break;
-            // if the choice is view all employees, call the view all employees method
+                // if the choice is view all employees, call the view all employees method
                 case 'viewAllEmployees':
                     viewAllEmployees();
                     break;
-            // if the choice is add department, call the add department method
+                // if the choice is add department, call the add department method
                 case 'addDepartment':
                     addDepartment();
                     break;
-            // if the choice is add role, call the add role method
+                // if the choice is add role, call the add role method
                 case 'addRole':
                     addRole();
                     break;
-            // if the choice is add employee, call the add employee method
+                // if the choice is add employee, call the add employee method
                 case 'addEmployee':
                     addEmployee();
                     break;
-            // if the choice is update employee role, call the update employee role method
+                // if the choice is update employee role, call the update employee role method
                 case 'updateEmployeeRole':
                     updateEmployeeRole();
                     break;
             }
         })
-}
-        // write functions that take user input and (create, update, delete) and calls the appropriate method
-        function viewAllDepartments() {
-            db.viewAllDepartments()
-                .then(([rows]) => {
-                    let departments = rows;
-                    console.log('\n');
-                    console.table(departments);
-                })
-                .then(() => questions());
-        }
-        
+};
+// write functions that take user input and (create, update, delete) and calls the appropriate method
+
+// creating view all functions query the database and return the results
+connection.query('SELECT * FROM department', function (err, results) {
+    if (err) throw err;
+    console.log(results);
+});
+
+connection.query('SELECT * FROM role', function (err, results) {
+    if (err) throw err;
+    console.log(results);
+});
+
